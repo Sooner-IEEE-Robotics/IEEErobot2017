f952c1df04adf846aa6637bffd944c34b397bdd9
jkleiber <jkleiber8@gmail.com>
2016-10-29 14:20:02 -0500

    I2C class ready to be tested

---
+++ Robot_Code/lib/I2C.js
@@ -0,0 +1,31 @@
+var bone = require('bonescript');
+
+//Instance variables for I2C
+var address = "";
+var iic = '/sys/class/i2c-adapter/i2c-1/';
+
+//Setup the I2C bus
+var I2C = function(deviceName, address)
+{
+	bone.writeTextFile(iic + 'new_device', deviceName);
+	this.address = address;
+};
+
+I2C.prototype.read = function(output)
+{
+	var data = "";
+	bone.readTextFile(this.address, function(x)
+	{
+		data = x.data;
+	});
+	
+	return data;
+}
+
+I2C.prototype.write = function(output)
+{
+	bone.writeTextFile(this.address, output)
+}
+
+//Exports the I2C module for global usage
+module.exports = I2C;
