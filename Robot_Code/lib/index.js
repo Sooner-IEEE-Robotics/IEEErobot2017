6cc5e4bf32aa092a00d6c647afa9087f2c00fe5c
jkleiber <jkleiber8@gmail.com>
2016-11-01 18:49:59 -0500

    Fixes problem with duplicate named PWM

--- Robot_Code/lib/index.js
+++ Robot_Code/lib/index.js
@@ -2,6 +2,6 @@ var bone = require('bonescript');
 
 module.exports.LED = require('./LED.js');
 module.exports.Button = require('./Button.js');
-module.exports.Button = require('./PWM.js');
+module.exports.PWM = require('./PWM.js');
 
 module.exports.LEDMatrix = require('./LEDMatrix.js');
