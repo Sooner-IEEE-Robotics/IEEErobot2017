a88d5b7d82d48eddbe9e2e8182d1d192fe2693c7
khaliddecroix <trevor.w.roach-1@ou.edu>
2017-02-23 14:48:52 -0600

    adds basic gyro and encoder support
    more fine tuning needed

--- Arduino Code/robot_mgr/robot_mgr.ino
+++ Arduino Code/robot_mgr/robot_mgr.ino
@@ -1,4 +1,8 @@
-
+#include "Wire.h"
+//A0 on MPU6050 set to 0
+//6050 7 bit address is 0x68
+//PWMGT address is 107; bit 6 controls sleep mode
+//z axis gryo is 71 and 72; low is 72, high is 71
 int left_motor_pin = 6, left_in_1 = 9, left_in_2 = 10;
 int right_motor_pin = 44, right_in_1 = 48, right_in_2 = 46;
 int leftEncoderA = 18;
@@ -7,81 +11,170 @@ int rightEncoderA = 3;
 int rightEncoderB = 4;
 int moving = 35;
 int instruct = 30;   
-
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+double gyro = 0;
 int E = 11, F = 12, G = 13;
-int leftEncoderPos = 0;
-int rightEncoderPos = 0;
+volatile int leftEncoderPos = 0;
+volatile int rightEncoderPos = 0;
 int Et = 0, Ft = 0, Gt = 0;
+double kR = 0.00395, kL = 0.00418;
+long int t = millis();
+double pos = 0;
+double distance = 0, rotation = 0;
 
+//************************GYRO BLOCK*******************************//
+double pollGyro(){
+ Wire.beginTransmission(MPU_addr);
+ Wire.write(71);  // starting with register 0x3B (ACCEL_XOUT_H)
+ Wire.endTransmission(false);
+ Wire.requestFrom(MPU_addr,2,true);  // request a total of 2 registers
+ gyro = (~((Wire.read()<<8 | Wire.read())-1));
+ //gyro = (((~(Wire.read()-1))<<8|(~(Wire.read()-1))));//*(250/32768));
+ //Serial.println(gyro);
+ return(gyro);//*(250.0/32768.0)-.05
+ //Serial.println();
+} 
+//************END GYRO BLOCK************************************//
 //**********************ENCODER BLOCK**************************************/
 void doLeftEncoder()
-            {
-              if(digitalRead(leftEncoderA) == digitalRead(leftEncoderB))
-              {
-                ++leftEncoderPos;
-              }
-              else
-              {
-                --leftEncoderPos;
-              }
+            {//Serial.println("HERE1");
+                leftEncoderPos++;
             }
 void doRightEncoder()
-            {
-              if(digitalRead(rightEncoderA) == digitalRead(rightEncoderB))
-              {
-                ++rightEncoderPos;
-              }
-              else
-              {
-                --rightEncoderPos;
-              }
+            {//Serial.println("HERE2");
+            rightEncoderPos++;
             }
 
 
 
-void resetEncoders()
-            {
-              leftEncoderPos = 0;
-              rightEncoderPos = 0;
-            }
+
 //**************************END ENCODER BLOCK ****************************************/
 
 //************************MOTOR BLOCK***************************//
-void mcu(int right, int left){                      //prototype mcu function
+void mcu(int dist, int rot){                      //prototype mcu function, dist in inches
            /*left_motor_pin     //enable/disable
            right_motor_pin    //enable/disable
            right_in_1
            right_in_2
            left_in_1
            left_in_2*/
+           float error = 0, errorI = 0;
+           int PID = 0;
+           float P = 1/50; 
+           distance = 0; 
+           rotation = 0;
+           int rotBase = 85;
+           int distBool = 0;
+           float I = 0.00005;
+           float testing = 100000;
            Serial.println("MCU");
-           digitalWrite(right_motor_pin, HIGH);     //enables the motors
-           digitalWrite(left_motor_pin, HIGH);
-           switch(right){
+           //digitalWrite(right_motor_pin, HIGH);     //enables the motors
+           //digitalWrite(left_motor_pin, HIGH);
+           leftEncoderPos = 0; rightEncoderPos = 0;
+           if(dist > 0) {distBool = 1;}
+           else if(dist < 0){distBool = -1;}
+           
+           //Serial.print("DIST: ");
+           //Serial.println(dist);
+           distance = dist / kR;
+           //Serial.print("DISTANCE: "); Serial.println(distance);
+           
+           //Serial.print("ERROR: "); Serial.println(error);
+           switch(distBool){
+              
               case -1:
-                  digitalWrite(right_in_1, HIGH);      //reverse
-                  digitalWrite(right_in_2, LOW);
+                  digitalWrite(right_in_1, LOW);      //reverse
+                  digitalWrite(right_in_2, HIGH);
+                  digitalWrite(left_in_1, LOW);                       
+                  
+                  digitalWrite(left_in_2, HIGH);
+                  distance = -distance;
+                  error = distance - leftEncoderPos;
+                  Serial.print("ENCODER1: "); 
+                  Serial.println(distance);
+                  Serial.print(leftEncoderPos); Serial.println("LEFT ENCODER POS");
+                  while(error > 0)
+                      {error = distance - leftEncoderPos;
+                       errorI += error; 
+                       PID = int(20 + P * error + I * errorI * error); 
+                       analogWrite(right_motor_pin, PID);
+                       analogWrite(left_motor_pin, PID);
+                       Serial.print("ENCODER1"); 
+                       Serial.println(error);
+                      }
+                  
+                  
                   break;
               case 1:
-                  digitalWrite(right_in_2,HIGH);      //forward
-                   digitalWrite(right_in_1,LOW);
+                   error = distance - leftEncoderPos;
+                   digitalWrite(right_in_1,HIGH);      //forward
+                   digitalWrite(right_in_2,LOW);
+                   digitalWrite(left_in_1, HIGH);                        
+                   digitalWrite(left_in_2, LOW);
+                   Serial.print("ENCODER2"); 
+                  Serial.println(distance);
+                   while(error > 0)
+                      {error = distance - leftEncoderPos;
+                       errorI += error; 
+                       PID = int(20 + P * error + I * errorI * error); 
+                       analogWrite(right_motor_pin, PID);
+                       analogWrite(left_motor_pin, PID);
+                       Serial.print("ENCODER2"); 
+                       Serial.println(error);
+                      }
+                   
+                  
                   break;
             
            }
-           switch(left){
-              case -1:
-                  digitalWrite(left_in_1, HIGH);       //reverse
+           switch(rot){
+              case -90:
+                  digitalWrite(right_in_1, LOW);      
+                  digitalWrite(right_in_2, HIGH);
+                  digitalWrite(left_in_1, HIGH);                        //left
                   digitalWrite(left_in_2, LOW);
+
+                   t = micros();
+                  error = rotBase + rotation;
+                    while(error > 0){
+                        rotation += float((micros()-t)*(pollGyro()-6)/testing)*(250.0/32768.0);//-.05;//double(micros() - t)/testing * pollGyro();  //gyro goes negative here
+                        t = micros();
+                        error = rotBase + rotation;
+                        errorI += error;
+                        PID = int(20 + error + I * errorI * error);
+                        if (PID > 255){PID = 255;}
+                        analogWrite(right_motor_pin, PID);
+                        analogWrite(left_motor_pin, PID);
+                        //Serial.println(rotation);
+                        delayMicroseconds(2000);
+                    }
                   break;
-              case 1:
-                  digitalWrite(left_in_1,LOW);       //forward
+              case 90:                                                //right
+                  digitalWrite(right_in_1, HIGH);      
+                  digitalWrite(right_in_2, LOW);
+                  digitalWrite(left_in_1,LOW);       
                   digitalWrite(left_in_2,HIGH);
+                  t = micros();
+                  error = rotBase - rotation;
+                    while(error > 0){
+                        rotation += float((micros()-t)*(pollGyro()-6)/testing)*(250.0/32768.0);//-.05;//double(micros() - t)/testing * pollGyro();
+                        t = micros();
+                        error = rotBase - rotation;
+                        errorI += error;
+                        PID = int(20 + error + I * errorI * error);
+                        if (PID > 255){PID = 255;}
+                        analogWrite(right_motor_pin, PID);
+                        analogWrite(left_motor_pin, PID);
+                        //Serial.println(rotation);
+                        delayMicroseconds(2000);
+                       
+                  }
                   break;
            }
-           delay(750);                             //allows the motors to run
+           //delay(750);                             //allows the motors to run
            digitalWrite(right_motor_pin, LOW);     //disables the motors when leaving the MCU
            digitalWrite(left_motor_pin, LOW);      //disabling motors after moving avoids the need for an explicit idle state
-           delay(1000);                            //allows time for the motors to turn off, makes individual states more visible
+           delay(2000);                            //allows time for the motors to turn off, makes individual states more visible
   
 }
 //********************END MOTOR BLOCK**********************************************//
@@ -91,20 +184,20 @@ void state_mgr(int instructions){
           
          
           switch(instructions){//program enters the state and does whatever action it was told to do; currently 8 states available
-              case 0:                         //state 0 
-                  mcu(1,1);                   //these are of course practice instructions
+              case 0:                         //forward 12" 
+                  mcu(12,0);                   //these are of course practice instructions
                   Serial.println("MCU 0");
                 break;
-              case 1:                         //state 1, etc
-                mcu(1,-1);
+              case 1:                         //backwards 12"
+                mcu(-12,0);
                 Serial.println("MCU 1");
                 break;
-              case 2:
-                mcu(-1,1);
+              case 2:                         //turn right; turn right is +90 degrees
+                mcu(0,90);
                 Serial.println("MCU 2");
                 break;
-              case 3:
-                mcu(-1,-1);
+              case 3:                         //turn left; turn left is -90 degrees
+                mcu(0,-90);
                 Serial.println("MCU 3");
                 break;
               case 4:
@@ -153,6 +246,16 @@ void setup() //Initilizes some pins
             digitalWrite(moving, HIGH); //tells AI that the robot is not moving at this time
             attachInterrupt(1, doRightEncoder, CHANGE); //pin 3 interrupt
             attachInterrupt(5, doLeftEncoder, CHANGE);  
+
+            Wire.begin();
+            Wire.beginTransmission(MPU_addr);
+            Wire.write(0x6B);  // PWR_MGMT_1 register
+            Wire.write(1);     // set to zero (wakes up the MPU-6050)
+            Wire.endTransmission(true);
+            Wire.beginTransmission(MPU_addr);
+            Wire.write(0x1B);  // Gyro
+            Wire.write(0x18);     // set to zero (wakes up the MPU-6050)
+            Wire.endTransmission(true);
             delay(5000);  //setup delay
 
 
@@ -163,6 +266,11 @@ void setup() //Initilizes some pins
 
 //*********************************************MAIN LOOP*********************************************
 void loop() {
+              
+              
+        
+  
+              
               
               //setup intializes automatically in the arduino ide
               while(digitalRead(instruct)==HIGH )    //VERY IMPORTANT, loops while there are no instructions present; exits loop when instructions are asserted
